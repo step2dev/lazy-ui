e7a33d7c36f248f5e10a6285f3fc4d9f80036868
CrazyBoy49z <yura.finiv@gmail.com>
2023-04-09 20:59:54 +0300

    Add FormInput component and its related view file for creating form inputs.

---
+++ src/Components/Form/FormInput.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Lazyadm\LazyComponent\Components\Form;
+
+use Illuminate\Contracts\View\View;
+use Lazyadm\LazyComponent\LazyComponent;
+
+class FormInput extends LazyComponent
+{
+    protected array $colors = [
+        'default'   => 'input-bordered',
+        'primary'   => 'input-primary',
+        'secondary' => 'input-secondary',
+        'accent'    => 'input-accent',
+        'ghost'     => 'input-ghost',
+        'info'      => 'input-info',
+        'success'   => 'input-success',
+        'warning'   => 'input-warning',
+        'error'     => 'input-error',
+        'danger'    => 'input-error',
+    ];
+
+    protected array $sizes = [
+        'default' => '',
+        'lg'      => 'input-lg',
+        'md'      => 'input-md',
+        'sm'      => 'input-sm',
+        'xs'      => 'input-xs',
+    ];
+    public ?string $placeholder;
+
+    public function __construct(public string $label = '', string $placeholder = '', public bool $required = false)
+    {
+        $this->placeholder = (string) str($placeholder ?: $this->label)->trim()->ucfirst();
+    }
+
+    public function render(): \Closure|View
+    {
+        return function (array $data) {
+            $data['attributes']['required'] = $this->required;
+
+            return view('lazy::form.input', $this->mergeData($data))->render();
+        };
+    }
+}

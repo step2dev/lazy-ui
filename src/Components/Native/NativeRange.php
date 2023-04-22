04a6530086dbb4459d6f670a769352de5abeedbf
CrazyBoy49z <CrazyBoy49z@users.noreply.github.com>
2023-04-22 21:09:31 +0000

    Fix styling

--- src/Components/Native/NativeRange.php
+++ src/Components/Native/NativeRange.php
@@ -8,22 +8,22 @@ use Lazyadm\LazyComponent\LazyComponent;
 class NativeRange extends LazyComponent
 {
     protected array $colors = [
-        'default'   => '',
-        'primary'   => 'range-primary',
+        'default' => '',
+        'primary' => 'range-primary',
         'secondary' => 'range-secondary',
-        'accent'    => 'range-accent',
-        'info'      => 'range-info',
-        'success'   => 'range-success',
-        'warning'   => 'range-warning',
-        'error'     => 'range-error',
+        'accent' => 'range-accent',
+        'info' => 'range-info',
+        'success' => 'range-success',
+        'warning' => 'range-warning',
+        'error' => 'range-error',
     ];
 
     protected array $sizes = [
         'default' => '',
-        'lg'      => 'range-lg',
-        'md'      => 'range-md',
-        'sm'      => 'range-sm',
-        'xs'      => 'range-xs',
+        'lg' => 'range-lg',
+        'md' => 'range-md',
+        'sm' => 'range-sm',
+        'xs' => 'range-xs',
     ];
 
     public function render(): \Closure|View

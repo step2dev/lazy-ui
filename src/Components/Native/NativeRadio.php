8ac5337073de386ef31907d152c78f3354809660
CrazyBoy49z <CrazyBoy49z@users.noreply.github.com>
2023-04-22 20:49:51 +0000

    Fix styling

--- src/Components/Native/NativeRadio.php
+++ src/Components/Native/NativeRadio.php
@@ -8,22 +8,22 @@ use Lazyadm\LazyComponent\LazyComponent;
 class NativeRadio extends LazyComponent
 {
     protected array $colors = [
-        'default'   => '',
-        'primary'   => 'radio-primary',
+        'default' => '',
+        'primary' => 'radio-primary',
         'secondary' => 'radio-secondary',
-        'accent'    => 'radio-accent',
-        'info'      => 'radio-info',
-        'success'   => 'radio-success',
-        'warning'   => 'radio-warning',
-        'error'     => 'radio-error',
+        'accent' => 'radio-accent',
+        'info' => 'radio-info',
+        'success' => 'radio-success',
+        'warning' => 'radio-warning',
+        'error' => 'radio-error',
     ];
 
     protected array $sizes = [
         'default' => '',
-        'lg'      => 'radio-lg',
-        'md'      => 'radio-md',
-        'sm'      => 'radio-sm',
-        'xs'      => 'radio-xs',
+        'lg' => 'radio-lg',
+        'md' => 'radio-md',
+        'sm' => 'radio-sm',
+        'xs' => 'radio-xs',
     ];
 
     public function render(): \Closure|View

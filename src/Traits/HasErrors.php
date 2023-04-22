d5f8bf787edbf0a24a9bb6dad60832ee5a88c722
CrazyBoy49z <yura.finiv@gmail.com>
2023-04-23 02:25:20 +0300

    Refactor HasErrors trait to fix null return value issue

--- src/Traits/HasErrors.php
+++ src/Traits/HasErrors.php
@@ -4,11 +4,10 @@ namespace Lazyadm\LazyComponent\Traits;
 
 trait HasErrors
 {
-    private ?bool $hasErrors = null;
+    protected ?bool $hasErrors = null;
 
     final public function hasErrors(): bool
     {
-//        $this->hasErrors ??= $this;
-        return false;
+        return $this->hasErrors ?? false;
     }
 }

22a3943749ea4922ee86c3bb2ffc14d5f45bbf2f
CrazyBoy49z <yura.finiv@gmail.com>
2023-08-06 04:13:08 +0300

    Add image mask

--- src/Components/Image.php
+++ src/Components/Image.php
@@ -21,6 +21,33 @@ class Image extends LazyComponent
                 'src' => $this->src,
                 'alt' => $this->alt,
             ]);
+
+            $mask = $attributes['mask'] ?? null;
+
+            $attributes = $this->mergeClasses($attributes, [
+                'mask'                 => $mask,
+                'mask-squircle'        => $mask === 'squircle',
+                'mask-heart'           => $mask === 'heart',
+                'mask-hexagon'         => $mask === 'hexagon',
+                'mask-hexagon-2'       => $mask === 'hexagon-2',
+                'mask-decagon'         => $mask === 'decagon',
+                'mask-pentagon'        => $mask === 'pentagon',
+                'mask-diamond'         => $mask === 'diamond',
+                'mask-square'          => $mask === 'square',
+                'mask-circle'          => $mask === 'circle',
+                'mask-parallelogram'   => $mask === 'parallelogram',
+                'mask-parallelogram-2' => $mask === 'parallelogram-2',
+                'mask-parallelogram-3' => $mask === 'parallelogram-3',
+                'mask-parallelogram-4' => $mask === 'parallelogram-4',
+                'mask-star'            => $mask === 'star',
+                'mask-star-2'          => $mask === 'star-2',
+                'mask-triangle'        => $mask === 'triangle',
+                'mask-triangle-2'      => $mask === 'triangle-2',
+                'mask-triangle-3'      => $mask === 'triangle-3',
+                'mask-triangle-4'      => $mask === 'triangle-4',
+                'mask-half-1'          => false,
+                'mask-half-2'          => false,
+            ]);
             $data['attributes'] = $attributes;
 
             return view('lazy::image', $this->mergeData($data))->render();

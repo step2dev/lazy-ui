b92207a216104fc8ff6df396160f06365847d0c7
CrazyBoy49z <yura.finiv@gmail.com>
2023-08-10 21:09:40 +0300

    Add BtnLogoutTest.php and BtnDeleteTest.php to the repository.

---
+++ tests/Components/BtnDeleteTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Lazyadm\LazyComponent\Tests\Components;
+
+use Lazyadm\LazyComponent\Components\Btn\BtnBack;
+
+it('can render button with attribute label', function () {
+    $this
+        ->blade('<x-lazy-btn-delete/>')
+        ->assertSee('class="btn', false);
+
+    $this
+        ->blade('<x-lazy-btn-back label="Go"/>')
+        ->assertSee('class="btn', false);
+
+    $this
+        ->blade('<x-lazy-btn-back label="Go"><i class="fas fa-arrow-left"></i></x-lazy-btn-back>')
+        ->assertSee('class="btn', false);
+})->todo();
+
+it('can render with sizes attribute', function ($size, $class) {
+    $this
+        ->blade("<x-lazy-btn-back {$size} />")
+        // ->assertSee($class)
+        ->assertSee('class="btn', false);
+
+    // $this->blade("<x-lazy-btn size='{$size}'>Go</x-lazy-btn>")
+    //     ->assertSee($class);
+})->with([
+    'lg' => ['lg', 'btn-lg'],
+    'md' => ['md', 'btn-md'],
+    'sm' => ['sm', 'btn-sm'],
+    'xs' => ['xs', 'btn-xs'],
+    '' => ['', ''],
+    // 'xl' => ['xl', 'btn-xl'],
+    // '2xl' => ['2xl', 'btn-2xl'],
+])->todo();
+
+it('can render with outline attribute', function () {
+    $this
+        ->blade('<x-lazy-btn-delete outline/>')
+        ->assertSee('btn-outline');
+});
+
+it('can render with disabled attribute', function () {
+    $this
+        ->blade('<x-lazy-btn-delete disabled/>')
+        ->assertSee('btn-disabled')
+        ->assertSee('disabled');
+});
---
+++ tests/Components/BtnLogoutTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Lazyadm\LazyComponent\Tests\Components;
+
+use Lazyadm\LazyComponent\Components\Btn\BtnBack;
+
+it('can render button with attribute label', function () {
+    $this
+        ->blade('<x-lazy-btn-logout/>')
+        ->assertSee('class="btn', false)
+        ->assertSee('href="#" onclick="javascript:"', false);
+
+    $this
+        ->blade('<x-lazy-btn-back label="Go"/>')
+        ->assertSee('href="#" onclick="javascript:"', false)
+        ->assertSee('Go');
+
+    $this
+        ->blade('<x-lazy-btn-back label="Go"><i class="fas fa-arrow-left"></i></x-lazy-btn-back>')
+        ->assertSee('class="btn', false);
+})->todo();
+
+it('can render with sizes attribute', function ($size, $class) {
+    $this
+        ->blade("<x-lazy-btn-back {$size} />")
+        // ->assertSee($class)
+        ->assertSee('class="btn', false);
+
+    // $this->blade("<x-lazy-btn size='{$size}'>Go</x-lazy-btn>")
+    //     ->assertSee($class);
+})->with([
+    'lg' => ['lg', 'btn-lg'],
+    'md' => ['md', 'btn-md'],
+    'sm' => ['sm', 'btn-sm'],
+    'xs' => ['xs', 'btn-xs'],
+    '' => ['', ''],
+    // 'xl' => ['xl', 'btn-xl'],
+    // '2xl' => ['2xl', 'btn-2xl'],
+])->todo();
+
+it('can render with outline attribute', function () {
+    $this
+        ->blade('<x-lazy-btn-logout outline/>')
+        ->assertSee('btn-outline');
+})->todo();
+
+it('can render with disabled attribute', function () {
+    $this
+        ->blade('<x-lazy-btn-logout disabled/>')
+        ->assertSee('btn-disabled')
+        ->assertSee('disabled');
+})->todo();

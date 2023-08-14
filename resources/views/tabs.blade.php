c5392e49422d18c0a02fd6293e2503d433a2653e
CrazyBoy49z <yura.finiv@gmail.com>
2023-08-14 22:50:51 +0300

    WIP

--- resources/views/tabs.blade.php
+++ resources/views/tabs.blade.php
@@ -3,42 +3,8 @@
     'size' => ''
 ])
 
-@dump($type)
-
-# Tabs
-<div {{ $attributes }}>
+<div {{ $attributes->class([
+    'tabs-boxed' => $type === 'boxed',
+]) }}>
     {{ $slot }}
-</div>
-
-Boxed
-<div class="tabs tabs-boxed">
-    <a class="tab">Tab 1</a>
-    <a class="tab tab-active">Tab 2</a>
-    <a class="tab">Tab 3</a>
-</div>
-
-Sizes
-<!-- xs -->
-<div class="tabs">
-    <a class="tab tab-xs tab-lifted">Tiny</a>
-    <a class="tab tab-xs tab-lifted tab-active">Tiny</a>
-    <a class="tab tab-xs tab-lifted">Tiny</a>
-</div>
-<!-- sm -->
-<div class="tabs">
-    <a class="tab tab-sm tab-lifted">Small</a>
-    <a class="tab tab-sm tab-lifted tab-active">Small</a>
-    <a class="tab tab-sm tab-lifted">Small</a>
-</div>
-<!-- md -->
-<div class="tabs">
-    <a class="tab tab-lifted">Normal</a>
-    <a class="tab tab-lifted tab-active">Normal</a>
-    <a class="tab tab-lifted">Normal</a>
-</div>
-<!-- lg -->
-<div class="tabs">
-    <a class="tab tab-lg tab-lifted">Large</a>
-    <a class="tab tab-lg tab-lifted tab-active">Large</a>
-    <a class="tab tab-lg tab-lifted">Large</a>
 </div>

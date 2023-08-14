cef785699f28b0d487464157d6edfb4ce1a4d3e9
CrazyBoy49z <yura.finiv@gmail.com>
2023-08-14 22:45:08 +0300

    WIP

--- resources/views/tabs.blade.php
+++ resources/views/tabs.blade.php
@@ -8,22 +8,8 @@
 # Tabs
 <div {{ $attributes }}>
     {{ $slot }}
-    <a class="tab">Tab 1</a>
-    <a class="tab tab-active">Tab 2</a>
-    <a class="tab">Tab 3</a>
 </div>
-Bordered
-<div class="tabs">
-    <a class="tab tab-bordered">Tab 1</a>
-    <a class="tab tab-bordered tab-active">Tab 2</a>
-    <a class="tab tab-bordered">Tab 3</a>
-</div>
-Lifted
-<div class="tabs">
-    <a class="tab tab-lifted">Tab 1</a>
-    <a class="tab tab-lifted tab-active">Tab 2</a>
-    <a class="tab tab-lifted">Tab 3</a>
-</div>
+
 Boxed
 <div class="tabs tabs-boxed">
     <a class="tab">Tab 1</a>

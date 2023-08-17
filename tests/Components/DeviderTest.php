89a67012f2639d161de3237e9f079e8e894144f4
CrazyBoy49z <yura.finiv@gmail.com>
2023-08-17 17:51:36 +0300

    Add DeviderTest.php with tests for rendering a lazy divider component.

---
+++ tests/Components/DeviderTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Lazyadm\LazyComponent\Tests\Components;
+
+it('can render Label', function () {
+    $this
+        ->blade('<x-lazy-divider />')
+        ->assertSee('divider', false);
+
+    $this
+        ->blade('<x-lazy-divider text="Divider Text"/>')
+        ->assertSee('divider', false)
+        ->assertSee('Divider Text');
+
+    $this
+        ->blade('<x-lazy-divider>Divider Text</x-lazy-divider>')
+        ->assertSee('divider', false)
+        ->assertSee('Divider Text');
+
+    $this
+        ->blade('Text before<x-lazy-divider text="Divider Text"/>Text after')
+        ->assertSee('divider', false)
+        ->assertSee('Divider Text')
+        ->assertSee('Text before')
+        ->assertSee('Text after');
+});

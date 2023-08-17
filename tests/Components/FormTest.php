af2c5bb89343330a4f1789086ee0be7e7c7afcf1
CrazyBoy49z <yura.finiv@gmail.com>
2023-08-17 17:59:02 +0300

    Add FormTest.php to the repository with form rendering test cases.

---
+++ tests/Components/FormTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Lazyadm\LazyComponent\Tests\Components;
+
+it('can render form', function () {
+    $this
+        ->blade('<x-lazy-form></x-lazy-form>')
+        ->assertSee('<form', false)
+        ->assertSee('method="POST"', false)
+        ->assertSee('type="hidden"', false)
+        ->assertSee('name="_token"', false);
+
+    $this
+        ->blade('<x-lazy-form method="PUT"></x-lazy-form>')
+        ->assertSee('<form', false)
+        ->assertSee('method="POST"', false)
+        ->assertSee('type="hidden"', false)
+        ->assertSee('name="_token"', false)
+        ->assertSee('<input type="hidden" name="_method" value="PUT">', false);
+
+    $this
+        ->blade('<x-lazy-form method="Delete" action="/test"></x-lazy-form>')
+        ->assertSee('<form', false)
+        ->assertSee('method="POST"', false)
+        ->assertSee('type="hidden"', false)
+        ->assertSee('name="_token"', false)
+        ->assertSee('<input type="hidden" name="_method" value="DELETE">', false)
+        ->assertSee('action="/test"', false);
+
+    $this
+        ->blade('<x-lazy-form method="GET" action="/test"></x-lazy-form>')
+        ->assertSee('<form', false)
+        ->assertSee('method="GET"', false)
+        ->assertSee('type="hidden"', false)
+        ->assertSee('name="_token"', false)
+        ->assertSee('action="/test"', false);
+});

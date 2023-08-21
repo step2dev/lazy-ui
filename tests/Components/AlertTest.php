09b613967ca71ca0255cded30cc49a878e32ddeb
CrazyBoy49z <yura.finiv@gmail.com>
2023-08-21 14:09:56 +0300

    Refactor AlertTest.php and AvatarGroupTest.php for better test coverage
    Revised the AlertTest.php to validate both 'label' and 'slot' parameters by splitting the test cases into 'For Label' and 'For Slot'. This ensures a more thorough testing of different alert scenarios.
    
    Similarly, updated the AvatarGroupTest.php to cater for scenarios with no children, single avatar, and multiple avatars respectively. These updates provide a more comprehensive testing of different avatar group formations for better code reliability.

--- tests/Components/AlertTest.php
+++ tests/Components/AlertTest.php
@@ -2,36 +2,25 @@
 
 namespace Lazyadm\LazyComponent\Tests\Components;
 
-it('can render alert with parameter', function ($parameter, $class) {
-    $this
-        ->blade("<x-lazy-alert {$parameter} label='Alert Content'/>")
+it('can render alert with parameter for both label and slot', function ($parameter, $class) {
+    // For Label
+    $this->blade("<x-lazy-alert {$parameter} label='Alert Content'/>")
         ->assertSee('alert')
         ->assertSee('Alert Content')
         ->assertSee($class);
 
-    $this
-        ->blade("<x-lazy-alert type='{$parameter}' label='Alert Content' />")
-        ->assertSee('alert')
-        ->assertSee('Alert Content')
-        ->assertSee($class);
-})->with([
-    'info' => ['info', 'alert-info'],
-    'success' => ['success', 'alert-success'],
-    'warning' => ['warning', 'alert-warning'],
-    'error' => ['error', 'alert-error'],
-    'default' => ['default', 'alert'],
-    '' => ['', 'alert'],
-]);
-
-it('can render alert with color and slot', function ($parameter, $class) {
-    $this
-        ->blade("<x-lazy-alert {$parameter}>Alert Content</x-lazy-alert>")
+    $this->blade("<x-lazy-alert type='{$parameter}' label='Alert Content' />")
         ->assertSee('alert')
         ->assertSee('Alert Content')
         ->assertSee($class);
 
-    $this
-        ->blade("<x-lazy-alert type='{$parameter}'>Alert Content</x-lazy-alert>")
+    // For Slot
+    $this->blade("<x-lazy-alert {$parameter}>Alert Content</x-lazy-alert>")
+        ->assertSee('alert')
+        ->assertSee('Alert Content')
+        ->assertSee($class);
+
+    $this->blade("<x-lazy-alert type='{$parameter}'>Alert Content</x-lazy-alert>")
         ->assertSee('alert')
         ->assertSee('Alert Content')
         ->assertSee($class);
--- tests/Components/AvatarGroupTest.php
+++ tests/Components/AvatarGroupTest.php
@@ -2,22 +2,32 @@
 
 namespace Lazyadm\LazyComponent\Tests\Components;
 
-it('can render avatar group', function () {
+it('can render avatar group without children', function () {
     $this
         ->blade('<x-lazy-avatar-group></x-lazy-avatar-group>')
         ->assertSee('avatar-group')
         ->assertSee('-space-x-6');
+});
 
+it('can render avatar group with a single avatar', function () {
     $this
         ->blade('<x-lazy-avatar-group>
-<x-lazy-avatar src="https://picsum.photos/200/200?random=1"/>
-<x-lazy-avatar class="w-10" alt="Avatar 1" src="https://picsum.photos/200/200?random=2"/>
-</x-lazy-avatar-group>')
+                    <x-lazy-avatar src="https://picsum.photos/200/200?random=1"/>
+                </x-lazy-avatar-group>')
         ->assertSee('avatar-group')
         ->assertSee('-space-x-6')
         ->assertSee('avatar')
-        ->assertSee('src="https://picsum.photos/200/200?random=1"', false)
-        ->assertSee('alt="Avatar"', false)
+        ->assertSee('src="https://picsum.photos/200/200?random=1"', false);
+});
+
+it('can render avatar group with multiple avatars', function () {
+    $this
+        ->blade('<x-lazy-avatar-group>
+                    <x-lazy-avatar src="https://picsum.photos/200/200?random=1"/>
+                    <x-lazy-avatar class="w-10" alt="Avatar 1" src="https://picsum.photos/200/200?random=2"/>
+                </x-lazy-avatar-group>')
+        ->assertSee('avatar-group')
+        ->assertSee('-space-x-6')
         ->assertSee('avatar')
         ->assertSee('w-10')
         ->assertSee('alt="Avatar 1"', false)
